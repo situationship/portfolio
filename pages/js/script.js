e7812227f967c2b83741c9afdb2e0a027cfb978e
Gina Chee <gc1979@nyu.edu>
2020-02-01 15:58:05 -0500

    fixed script

--- pages/js/script.js
+++ pages/js/script.js
@@ -22,13 +22,6 @@ $(document).ready(function(){
 jQuery(document).ready(function() {
         var offset = 220;
         var duration = 500;
-        jQuery(window).scroll(function() {
-            if (jQuery(this).scrollTop() > offset) {
-                jQuery('.back-to-top').fadeIn(duration);
-            } else {
-                jQuery('.back-to-top').fadeOut(duration);
-            }
-        });
         jQuery('.back-to-top').click(function(event) {
             event.preventDefault();
             jQuery('html, body').animate({scrollTop: 0}, duration);

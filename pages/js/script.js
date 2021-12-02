f33e7873a7cbbe5c647005e6b5b4193470616a83
gina-chee <gina.chee@gmail.com>
2021-12-02 17:30:12 -0500

    minor fixes

--- pages/js/script.js
+++ pages/js/script.js
@@ -52,9 +52,9 @@ window.addEventListener('DOMContentLoaded', () => {
         entries.forEach(entry => {
             const id = entry.target.getAttribute('id');
             if (entry.intersectionRatio > 0) {
-                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+                document.querySelector(`li a[href="#${id}"]`).parentElement.classList.add('active');
             } else {
-                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+                document.querySelector(`li a[href="#${id}"]`).parentElement.classList.remove('active');
             }
         });
     });

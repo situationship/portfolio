5c137586ee2691f9aba4ce291fcc87093687d365
Gina Chee <ginachee@Ginas-MBP.lan>
2024-03-07 18:02:46 -0500

    fixed masonry, updated year

--- pages/js/script.js
+++ pages/js/script.js
@@ -103,7 +103,6 @@ new Freezeframe('.freezeframe-dontstop', {
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   percentPosition: true,
-  columnWidth: '.grid-sizer'
 });
 // layout Masonry after each image loads
 $grid.imagesLoaded().progress( function() {
